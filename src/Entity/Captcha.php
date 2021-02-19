865f8f8b5c8cb0c175d268e06f5fa7b7a48928b8
azerpas <xskylix@hotmail.fr>
2021-02-19 13:19:43 +0100

    Captcha entity

---
+++ src/Entity/Captcha.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CaptchaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CaptchaRepository::class)
+ */
+class Captcha
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $givenId;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $answer;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $addedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGivenId(): ?string
+    {
+        return $this->givenId;
+    }
+
+    public function setGivenId(string $givenId): self
+    {
+        $this->givenId = $givenId;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?string
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(string $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    public function getAddedAt(): ?\DateTimeInterface
+    {
+        return $this->addedAt;
+    }
+
+    public function setAddedAt(\DateTimeInterface $addedAt): self
+    {
+        $this->addedAt = $addedAt;
+
+        return $this;
+    }
+}
